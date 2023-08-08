105fb100e50280c8f3e48f5acc574b36d984791b
Dcastillomar <125750537+Dcastillomar@users.noreply.github.com>
2023-08-08 14:03:24 -0500

    file moves test

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PortfolioContainer from "./src/components/PortfolioContainer";
+import Header from './src/components/Header';
+import Footer from './src/components/Footer';
+
+
+
+const App = () =>
+    <div>
+        <Header />
+        <PortfolioContainer />
+        <Footer />
+    </div>
+
+export default App;
+
+
