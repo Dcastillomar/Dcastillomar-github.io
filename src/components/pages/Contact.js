322e13df7f0fd1040dbbca71aae749d7d9ed10eb
Dcastillomar <125750537+Dcastillomar@users.noreply.github.com>
2023-08-08 13:25:54 -0500

    add alerts to name,email and message fields

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -21,7 +21,8 @@ function Form() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
-
+  const [error, setError] = useState({type: null, message: null});
+  console.log(error)
   const handleInputChange = (e) => {
     const { target } = e;
     const inputType = target.name;
@@ -35,6 +36,29 @@ function Form() {
       setMessage(inputValue);
     }
   };
+  const checkForm = (e) => {
+    console.log(error)
+   
+    if (e.target.name === "name"){
+      if (e.target.value === ""){
+        setError({type: "name", message: "Field is Required"})
+        return
+      }
+    }
+    if (e.target.name === "message"){
+      if (e.target.value === ""){
+        setError({type: "message", message: "Field is Required"})
+        return
+      }
+    } 
+    if (e.target.name === "email"){
+      if (!validateEmail(e.target.value)){
+        console.log("Invalid Email")
+        setError({type: "email", message: "Invalid Email"})
+        return
+      }
+    }
+  }
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -65,9 +89,13 @@ function Form() {
       
       <form className="form-control">
       <div class="col-md-6">
+      
       <label for="inputName" class="form-label">Name</label>
+      {error.type === "name" ? <p>{error.message}</p> : ""}
       <br></br>
         <input
+         onFocus={() => setError({type: null, message: null})}
+         onBlur={(e) => checkForm(e)}
           value={name}
           name="name"
           onChange={handleInputChange}
@@ -75,9 +103,13 @@ function Form() {
           placeholder="John Doe"
         /></div>
         <div class="col-md-6">
-        <label for="inputName" class="form-label">Email</label>
+         
+        <label for="inputName" class="form-label">Email</label> 
+        {error.type === "email" ? <p>{error.message}</p> : ""}
         <br></br>
         <input
+          onFocus={() => setError({type: null, message: null})}
+          onBlur={(e) => checkForm(e)}
           value={email}
           name="email"
           onChange={handleInputChange}
@@ -85,13 +117,17 @@ function Form() {
           placeholder="username@email.com"
         /></div>
          <div class="col-12">
-         <label for="inputName" class="form-label">Message</label>
+        
+         <label for="inputName" class="form-label">Message</label> 
+         {error.type === "message" ? <p>{error.message}</p> : ""}
         <br></br>
         <textarea
-          value={message}
+        onFocus={() => setError({type: null, message: null})}
+        onBlur={(e) => checkForm(e)}
+        value={message}
           name="message"
           onChange={handleInputChange}
-          type="text"
+          type="message"
           placeholder="Message Here"
         /></div>
         <button type="button" class="btn btn-primary" onClick={handleFormSubmit}>Submit</button>
